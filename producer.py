6f1a9621e87432a69a47562c4b22dcc5e6b6cdc6
Гут Владимир <vladimir.monolith@yandex.ru>
2025-01-14 18:52:26 +0300

    Initial commit

---
+++ producer.py
@@ -0,0 +1,22 @@
+from pika import BlockingConnection, ConnectionParameters
+
+connection_params = ConnectionParameters(
+    host='localhost',
+    port=5762
+)
+
+
+def main():
+    with BlockingConnection(connection_params) as connection:
+        with connection.channel() as channel:
+            channel.queue_declare(queue='messages')
+            channel.basic_publish(
+                exchange='',  # дефолтная очередь
+                routing_key='messages',
+                body='Тестовое сообщение RabbitMQ.'
+            )
+            print('Сообщение отправлено.')
+
+
+if __name__ == '__main__':
+    main()
